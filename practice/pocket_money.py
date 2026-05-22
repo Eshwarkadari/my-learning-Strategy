491d1f4a97d37ce0b8dd2778441d92982da658de
KADARI ESHWAR <eshwarkadari78@gmail.com>
2026-05-23 00:30:20 +0530

    Add pocket_money.py

---
+++ practice/pocket_money.py
@@ -0,0 +1,5 @@
+# Pocket Money Calculator
+daily = float(input("Enter daily pocket money: "))
+days = int(input("Enter number of days: "))
+total = daily * days
+print(f"Total pocket money for {days} days = {total}")
