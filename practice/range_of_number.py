b9f14ec012f5013b024eaff3bd5e525830cf4a11
KADARI ESHWAR <eshwarkadari78@gmail.com>
2026-05-23 00:30:06 +0530

    Add range of number.py from RANGE-OF-NUMBER

---
+++ practice/range_of_number.py
@@ -0,0 +1,12 @@
+n=int(input("Enter a number:"))
+if 1<=n<=50:
+    print("Range in 1 to 50")
+elif 51<=n<=100:
+    print("Range in 51 to 100")
+elif 101<=n<=150:
+    print("Range in 101 to 150")
+elif 151<=n<=200:
+    print("Range in 151 to 200")
+else :
+    print("Enter a num btw 1to 200")
+    
